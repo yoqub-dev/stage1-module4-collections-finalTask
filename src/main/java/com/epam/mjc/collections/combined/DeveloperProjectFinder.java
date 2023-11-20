0bd80b140b49184c13a54d5cc136ecda66342ce5
yoqub-dev <yoqub.epam@gmail.com>
2023-11-20 10:48:53 +0500

    task

--- src/main/java/com/epam/mjc/collections/combined/DeveloperProjectFinder.java
+++ src/main/java/com/epam/mjc/collections/combined/DeveloperProjectFinder.java
@@ -1,10 +1,26 @@
 package com.epam.mjc.collections.combined;
 
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public class DeveloperProjectFinder {
     public List<String> findDeveloperProject(Map<String, Set<String>> projects, String developer) {
+        List<String> developerProjects = new ArrayList<>();
+        Set<String> projectsForDeveloper = projects.getOrDefault(developer, new HashSet<>());
+
+        for (Map.Entry<String, Set<String>> entry : projects.entrySet()) {
+            if (entry.getValue().contains(developer)) {
+                developerProjects.add(entry.getKey());
+            }
+        }
+
+        developerProjects.sort((a, b) -> {
+            if (a.length() != b.length()) {
+                return Integer.compare(b.length(), a.length());
+            } else {
+                return b.compareTo(a);
+            }
+        });
+
+        return developerProjects;
     }
 }
--- src/main/java/com/epam/mjc/collections/combined/MapFromKeysCreator.java
+++ src/main/java/com/epam/mjc/collections/combined/MapFromKeysCreator.java
@@ -1,9 +1,22 @@
 package com.epam.mjc.collections.combined;
 
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public class MapFromKeysCreator {
     public Map<Integer, Set<String>> createMap(Map<String, Integer> sourceMap) {
+        Map<Integer, Set<String>> resultMap = new HashMap<>();
+
+        for (Map.Entry<String, Integer> entry : sourceMap.entrySet()) {
+            int keyLength = entry.getKey().length();
+            String key = entry.getKey();
+
+            if (!resultMap.containsKey(keyLength)) {
+                resultMap.put(keyLength, new TreeSet<>(Collections.reverseOrder()));
+            }
+
+            resultMap.get(keyLength).add(key);
+        }
+
+        return resultMap;
     }
 }
